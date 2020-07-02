a51b3a450c2a701411d01a42774d0959810b92a6
Yaroslav Ermilov <ya.ermilov@gmail.com>
2020-07-02 22:51:56 +0300

    Delete commented code of example

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,1 @@
 // Этот скрипт выполняется в фоне браузера
-
-/*
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        var a = request.site; // данные о сайте
-        var b = request.time; // данные о проведенном времени
-        // тут делаем с этими данными что хотим.
-    });
-*/
--- src/js/content.js
+++ src/js/content.js
@@ -1,19 +1,1 @@
 /* Этот скрипт выполняется на разрешённых страницах */
-
-// window.onblur = function() { // окно теряет фокус
-//     chrome.runtime.sendMessage({ site: site, time: localStorage[site] }); // отправка сообщения на background.js
-//     localStorage[site] = '0';
-// }
-
-// function sec() //выполняется каждую секунду
-// {
-//     // if (document.webkitVisibilityState == 'visible') //если страница активна
-//     // {
-//     //     localStorage[site] = parseInt(localStorage[site], 10) + 1; // обновляем данные о сайте в локальном хранилище
-//     // }
-//     console.log("Hello world");
-
-// }
-
-// setInterval(sec, 5000); // запускать функцию каждые 5 сек
-
