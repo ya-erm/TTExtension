e156a5631a5b85fe9b648504be0d146082378114
Yaroslav Ermilov <ya.ermilov@gmail.com>
2021-02-19 23:20:16 +0300

    Fix partial executed fill quantity

--- src/js/fill.js
+++ src/js/fill.js
@@ -5,6 +5,7 @@ export class Fill {
         this.operationType = item.operationType;
         this.price = item.price;
         this.quantity = item.quantity;
+        this.quantityExecuted = item.quantityExecuted;
         this.payment = item.payment;
         this.commission = item.commission?.value;
     }
